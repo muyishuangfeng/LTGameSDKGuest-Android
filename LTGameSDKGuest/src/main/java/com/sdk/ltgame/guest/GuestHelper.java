bb174ab3f8fd6568ebb8dd87946c86cd4b57b2c3
silence <yangkemuyi@sina.com>
2019-12-26 21:22:50 +0800

    增加解绑功能

--- LTGameSDKGuest/src/main/java/com/sdk/ltgame/guest/GuestHelper.java
+++ LTGameSDKGuest/src/main/java/com/sdk/ltgame/guest/GuestHelper.java
@@ -12,6 +12,7 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.FacebookSdkNotInitializedException;
 import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.gentop.ltgame.ltgamesdkcore.common.Target;
 import com.gentop.ltgame.ltgamesdkcore.impl.OnLoginStateListener;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -19,11 +20,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.sdk.ltgame.ltnet.manager.LoginRealizeManager;
 
 import java.lang.ref.WeakReference;
 import java.util.Arrays;
+
 
 public class GuestHelper {
 
@@ -67,12 +70,18 @@ public class GuestHelper {
 
 
     /**
+     * 解绑账户
+     */
+    private void unBindAccount() {
+        LoginRealizeManager.unBindAccount(mActivityRef.get(), mListener);
+    }
+
+    /**
      * 游客登录
      */
     private void guestLogin() {
         LoginRealizeManager.guestLogin(mActivityRef.get(), mListener);
     }
-
 
 
     /**
@@ -85,7 +94,7 @@ public class GuestHelper {
     /**
      * 登录或者绑定
      */
-    void guestLogin( String result, String appID) {
+    void guestLogin(String result, String appID) {
         if (!TextUtils.isEmpty(result)) {
             if (TextUtils.equals(result, "1")) {//游客
                 guestLogin();
@@ -93,18 +102,26 @@ public class GuestHelper {
                 initFaceBook(appID);
             } else if (TextUtils.equals(result, "3")) {//绑定Google
                 bindGoogle();
+            }else if (TextUtils.equals(result, "4")) {//解绑账户
+                unBindAccount();
             }
         }
     }
 
-    private void initGoogle(Activity context, String clientID, int selfRequestCode) {
+    private void initGoogle(final Activity context, String clientID, final int selfRequestCode) {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(clientID)
                 .requestEmail()
                 .build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(context, gso);
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        context.startActivityForResult(signInIntent, selfRequestCode);
+        final GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(context, gso);
+        mGoogleSignInClient.signOut().addOnCompleteListener((Activity) context, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                context.startActivityForResult(signInIntent, selfRequestCode);
+            }
+        });
+
     }
 
 
@@ -135,13 +152,14 @@ public class GuestHelper {
     private void initFaceBook(String mFacebookID) {
         FacebookSdk.setApplicationId(mFacebookID);
         FacebookSdk.sdkInitialize(mActivityRef.get());
+        LoginManager.getInstance().logOut();
         try {
             mFaceBookCallBack = CallbackManager.Factory.create();
             LoginManager.getInstance()
                     .logInWithReadPermissions(mActivityRef.get(),
                             Arrays.asList("public_profile"));
             LoginManager.getInstance().registerCallback(mFaceBookCallBack,
-                    new FacebookCallback<com.facebook.login.LoginResult>() {
+                    new FacebookCallback<LoginResult>() {
                         @Override
                         public void onSuccess(com.facebook.login.LoginResult loginResult) {
                             if (loginResult != null) {
@@ -168,9 +186,6 @@ public class GuestHelper {
         }
 
     }
-
-
-
 
 
 }
